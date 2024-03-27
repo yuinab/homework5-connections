2c8a19063c8dca1f50794c8f401f242c895d42b3
drewhollar1 <mhc3cm@virginia.edu>
2024-03-26 22:39:23 -0400

    Update index.php
    changed the layout still works the same

--- index.php
+++ index.php
@@ -3,6 +3,7 @@ session_start();
 $request = $_GET['command'] ?? '';
 
 switch ($request) {
+    // For starting the game
     case 'startGame':
         // Process the form data
         $_SESSION['name'] = trim($_POST['name']);
@@ -10,28 +11,35 @@ switch ($request) {
         header('Location: views/game.php');
         exit();
         break;
+    
+    // For exiting the game
+    case 'exitGame':
+        // Clear
+        $_SESSION = array();
+
+        // Delete cookie
+        if (ini_get("session.use_cookies")) {
+            $params = session_get_cookie_params();
+            setcookie(session_name(), '', time() - 42000,
+                $params["path"], $params["domain"],
+                $params["secure"], $params["httponly"]
+            );
+        }
+        session_destroy();
+
+        header('Location: index.php?command=welcome');
+        exit();
+        break;
+    
+    // No specific command show the welcome page if not already playing
     default:
-        // No specific command, show the welcome page if not already playing
         if (!isset($_SESSION['name']) || !isset($_SESSION['email'])) {
             include('views/welcome.php');
         } else {
-            // User has already entered details, redirect to the main game page
+            // User has already entered details redirect to the main game page
             header('Location: views/game.php');
             exit();
         }
         break;
-    case 'exitGame':
-    $_SESSION = array();
-
-    if (ini_get("session.use_cookies")) {
-        $params = session_get_cookie_params();
-        setcookie(session_name(), '', time() - 42000,
-            $params["path"], $params["domain"],
-            $params["secure"], $params["httponly"]
-        );
-    }
-
-    session_destroy();
-    header('Location: index.php');
-    exit();
 }
+?>
